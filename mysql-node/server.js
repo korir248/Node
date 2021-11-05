928f0b625e2d5bbaab8377e399a1c67df33596f4
Eugene <korir16071@gmail.com>
2021-11-05 12:55:59 +0300

    Modified server.js adding delete method

--- mysql-node/server.js
+++ mysql-node/server.js
@@ -21,24 +21,65 @@ app.get("/",(req,res)=>{
      res.status(200).send("Welcome Home!")
 })
 
-app.get("/users")
+app.get("/users",(req,res)=>{
+    let sql = 'select * from user_data'
+    db.query(sql, (err,result)=>{
+        if (err) {
+            console.log(err.message);            
+            throw err
+        }else{
+            console.log(result);
+            res.status(200).send(result)
+        }
+    })
+});
+
+app.get("/users/:id", (req,res)=>{
+    const {id} = req.params
+    let sql = `select * from user_data where id = ${id}`
+    db.query(sql,(err,result)=>{
+        if (err) {
+            throw err          
+        }else{
+            res.status(200).send(result)
+            console.log(result);
+        }
+    })
+
+})
 
 
 app.post("/signup",(req,res)=>{
     const {username, email, password} = req.body
     let sql = `INSERT INTO user_data(username,email,password) VALUES ('${username}', '${email}', '${password}')`
+    let sql2 = `select * from user_data where username = ${username}`
 
-    db.query(sql,(err,result)=>{
+    db.query(sql2,(err,result)=>{
+        console.log(result);
         if(err){
-            console.log(err.message);
-            throw err
-            
+            db.query(sql,(err,result)=>{
+                if(err) {
+                    res.status(200).send(`User ${username} added successfully!`)
+                    console.log(`User ${username} added successfully`);
+                }             
+            })
+
         }else{
-            res.status(200).send("User added successfully")
-        console.log("User added successfully ");
 
         }
     }) 
+})
+app.delete("/users/:id", (req,res)=>{
+    const {id} = req.params
+    let sql = `delete from user_data where id = ${id}`
+    db.query(sql,(err,result)=>{
+        if(err){
+            res.status().send(`Could not delete user with id ${id}`)
+            throw err
+        }else{
+            res.status()
+        }
+    })
 })
 
 app.listen(port,()=>{
