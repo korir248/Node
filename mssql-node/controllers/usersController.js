f4750ed05d9eac86bf5db22528e4d0393e9f44a3
Eugene Korir <korir16071@gmail.com>
2021-11-15 09:25:10 +0300

    Added deleteUser and updateUser controllers

--- mssql-node/controllers/usersController.js
+++ mssql-node/controllers/usersController.js
@@ -4,13 +4,17 @@ const config = require('../config/db.config')
 const getUsers = async()=> {
      try {
         let pool= await mssql.connect(config)
-        let sql = 'select id, username, email from users where isDeleted = 0';
+        let sql = 'select id, username, email,isDeleted from users where isDeleted = 0';
         let users = await pool.request().query(sql)
+        console.log(users.recordset.length)
+
         const u =users.recordset.map(user=>{
+            
             return {
                 user_id: user.id,
                 username: user.username.trim(),
-                email: user.email.trim()
+                email: user.email.trim(),
+                isDeleted: user.isDeleted
             }
         })
         return u
@@ -24,7 +28,11 @@ const getUsers = async()=> {
        let pool= await mssql.connect(config)
        let sql = `select id, username, email from users where isDeleted = 0 and id =${id}`;
        let users = await pool.request().query(sql)
-       const u =users.recordset.map(user=>{
+       if(users.recordset.length === 0){
+           return `No user with id ${id} was found!`
+       }
+
+       const u =users.recordset.map(user=>{          
            return {
                user_id: user.id,
                username: user.username.trim(),
@@ -36,5 +44,48 @@ const getUsers = async()=> {
         console.log(err.message);
     }
 }
+const deleteUser = async(id)=>{
+    try {
+        let pool = await mssql.connect(config)
+        let sql = `update users set isDeleted = 1 where id = ${id}`
+        let result = await pool.request().query(sql,(err,result)=>{
+            if (err) {
+                console.log(err.message);
+                return err.message              
+            }else{
+                
+            }
+        })
+    } catch (error) {
+        console.log(error.message);        
+    }
+}
 
- module.exports = {getUsers, getSpecificUser}
+const updateUser = async(id,username)=>{
+    try {
+        let pool = await mssql.connect(config)
+        let sql = `update users set username = ${username} where id = ${id}`
+        let result = await pool.request().query(sql,(err,result)=>{
+            if (err) {
+                console.log(err.message);
+                return err.message              
+            }else{
+                
+            }
+        })
+    } catch (error) {
+        console.log(error.message);        
+    }
+
+}
+
+const addUser = async(username,email,password)=>{
+try {
+    let pool = await mssql.connect(config)
+let sql = `insert into users(username,email,password) values(${username},${email},${password})`
+    
+} catch (err) {
+console.log(err.message);
+}
+}
+ module.exports = {getUsers, getSpecificUser,deleteUser,updateUser}
